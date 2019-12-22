5cf5c5b674a4b6c79fbdcfddade52b880927c344
Nahid <nahid.ahamed.qa@gmail.com>
2019-12-22 10:52:56 -0500

    inherits

---
+++ src/com/javaBasic/InheritOne.java
@@ -0,0 +1,16 @@
+package com.javaBasic;
+
+public class InheritOne {
+
+
+    static int banana = 30;
+    static int orange = 20;
+    static int total = banana+ orange;
+
+    public  static void test (){
+
+
+        System.out.println(total);
+
+    }
+}
---
+++ src/com/javaBasic/InheritTwo.java
@@ -0,0 +1,12 @@
+package com.javaBasic;
+
+public class InheritTwo extends InheritOne{
+
+
+    public static void main(String[] args) {
+
+        test();
+
+    }
+
+}
