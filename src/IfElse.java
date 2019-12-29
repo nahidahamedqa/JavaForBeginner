7aa6dab7b2a3134c5e9848dada28e7c95dd6c79c
Nahid <nahid.ahamed.qa@gmail.com>
2019-12-29 11:08:41 -0500

    IfElse

---
+++ src/IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+
+
+    public static void main (String[]args){
+
+        int salt1 = 10;
+        int salt2 = 5;
+
+        if (salt1 < 20) {
+
+            System.out.println(salt1);
+        } else {
+          System.out.println(salt2);
+        }
+    }
+}
